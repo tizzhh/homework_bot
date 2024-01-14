6a50176ac9534d8d2f3a43deb52efb1c2f2e539f
Vadim Kuznetsov <darovadraste@gmail.com>
2024-01-14 19:13:21 +0300

    Finish review fixes: add decorator for last sent message check: .

--- homework.py
+++ homework.py
@@ -3,10 +3,10 @@ import os
 import sys
 import time
 from contextlib import suppress
+from functools import wraps
 from http import HTTPStatus
 from logging import StreamHandler
 from typing import Union
-from functools import wraps
 
 import requests
 import telegram
@@ -14,6 +14,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Забыл спросить. Я так понимаю, логгер следует использовать
+# вообще в любом проекте адекватном. В django_sprint4, например
+# он бы тоже не помешал, да?
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = StreamHandler(stream=sys.stdout)
@@ -57,41 +60,57 @@ def check_tokens() -> None:
         sys.exit(message)
 
 
+# Благодаря твоему комментарию вспомнил вообще
+# про существование декораторов, поэтому решил
+# добавить такой тоже. По-моему, при масштабировании
+# как раз будет кстати.
 def logger_debug(f):
     """Декоратор для логирования уровня DEBUG."""
+
     @wraps(f)
     def wrapper(*args, **kwargs):
         logger.debug(
             f'Начало выполнения {f.__name__}'
-            f'{args}, {kwargs}'
+            f'с args: {args}, kwargs: {kwargs}'
         )
         output = f(*args, **kwargs)
         logger.debug(
-            f'Конец выполнения {f.__name__}'
-            f'{args}, {kwargs}'
+            f'Конец выполнения {f.__name__}' f'с args: {args}, {kwargs}'
         )
         return output
+
     return wrapper
 
 
+def was_this_message_already_sent(f):
+    """Декоратор для проверки последнего отправленного сообщения."""
+    last_msg = ''
+
+    @wraps(f)
+    # как-то не придумал, как можно сделать с args и kwargs,
+    # поэтому явно аргументы оставил
+    def wrapper(bot, message):
+        nonlocal last_msg
+        if message != last_msg:
+            last_msg = message
+            return f(bot, message)
+        logger.debug(f'Было получено повторяющееся сообщение: {message}')
+
+    return wrapper
+
+
+@was_this_message_already_sent
+@logger_debug
 def send_message(bot: telegram.Bot, message: str) -> None:
     """Функиця для отправки ботом сообщения."""
-    logger.debug(
-        f'Начало отправки сообщения юзеру {TELEGRAM_CHAT_ID}'
-        f'с текстом {message}'
-    )
     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-    logger.debug(
-        f'Отправлено сообщение юзеру {TELEGRAM_CHAT_ID} '
-        f'с текстом {message}'
-    )
 
 
+@logger_debug
 def get_api_answer(
         timestamp: int,
 ) -> dict[str, Union[list[dict[str, Union[str, int]]], int]]:
     """Функция для получения ответа от API."""
-    logger.debug(f'Начало отправки запроса с параметром {timestamp}')
     try:
         response = requests.get(
             ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
@@ -104,13 +123,12 @@ def get_api_answer(
             f'Эндпоинт {ENDPOINT} недоступен. '
             f'Код ответа API: {response.status_code}'
         )
-    logger.debug(f'Успешно получен ответ на запрос с параметром {timestamp}')
     return response.json()
 
 
+@logger_debug
 def check_response(response: requests.Response) -> None:
     """Функция для проверки наличия необходимых ключей в ответе."""
-    logger.debug('Начало проверки ключей в ответе API')
     if not isinstance(response, dict):
         raise TypeError(f'API вернул не словарь, а {type(response)}')
     if 'homeworks' not in response:
@@ -120,12 +138,11 @@ def check_response(response: requests.Response) -> None:
             f'Под ключом homeworks находится не список,'
             f'а {type(response["homeworks"])}'
         )
-    logger.debug('Успешное завершение проверки ключей в ответе API')
 
 
+@logger_debug
 def parse_status(homework: dict[str, Union[int, str]]) -> str:
     """Функция для парсинга статуса проверки домашней работы."""
-    logger.debug(f'Начало парсинга статуса для словаря {homework}')
     if 'homework_name' not in homework:
         raise KeyError('Отсутствие ключа homework_name в ответе API')
     if 'status' not in homework:
@@ -133,9 +150,6 @@ def parse_status(homework: dict[str, Union[int, str]]) -> str:
     recieved_status = homework['status']
     if recieved_status not in HOMEWORK_VERDICTS:
         raise ValueError(f'Неожиданный статус домашки: {recieved_status}')
-    logger.debug(
-        f'Успешное завершение парсинга статуса для словаря {homework}'
-    )
     return (
         f'Изменился статус проверки работы "{homework["homework_name"]}".'
         f'{HOMEWORK_VERDICTS[homework["status"]]}'
@@ -147,7 +161,6 @@ def main() -> None:
     check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-    last_msg = ''
     while True:
         try:
             response = get_api_answer(timestamp - RETRY_PERIOD)
@@ -157,8 +170,7 @@ def main() -> None:
                 logger.info('Обновления отсутствуют')
                 continue
             message = parse_status(response['homeworks'][0])
-            if message != last_msg:
-                last_msg = send_message(bot, message)
+            send_message(bot, message)
             timestamp = response['current_date']
         except telegram.error.TelegramError as error:
             logger.error(f'Сбой в работе телеграмма: {error}')
@@ -166,8 +178,7 @@ def main() -> None:
             error_msg = f'Сбой в работе программы: {error}'
             logger.error(error_msg, exc_info=True)
             with suppress(telegram.error.TelegramError):
-                if error_msg != last_msg:
-                    last_msg = send_message(bot, error_msg)
+                send_message(bot, error_msg)
         finally:
             time.sleep(RETRY_PERIOD)
 
